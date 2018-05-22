5fb6bec04df090bb895e5acfc7d284598d0c8a26
Alan Oliveira <alan.sil.oliveira@gmail.com>
2018-05-22 11:07:05 -0300

    Falta fazer o menu, e solicitar na classe principal

--- ProjetoBombeiro/src/RegistroEdificio.java
+++ ProjetoBombeiro/src/RegistroEdificio.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import javax.swing.JOptionPane;
 
@@ -12,19 +7,39 @@ import javax.swing.JOptionPane;
  */
 public class RegistroEdificio {
 
-    int qnt = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de solicitações: "));
-    String[] nome = new String[qnt];
-    String[] cpf = new String[qnt];
-    String[] cidade = new String[qnt];
-    String[] rua = new String[qnt];
-    String[] cep = new String[qnt];
-    String[] cnpj = new String[qnt];
-    String[] edificio = new String[qnt];
+    //int qnt = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de solicitações: "));
+    String[] nome = new String[100];
+    String[] cpf = new String[100];
+    String[] cidade = new String[100];
+    String[] rua = new String[100];
+    String[] cep = new String[100];
+    String[] cnpj = new String[100];
+    String[] nomeEdificio = new String[100];
+    String[] edificio = new String[100];
     int atual = 0;
 
+//    public void menuRegistroEdificio() {
+//        int escolha = Integer.parseInt(JOptionPane.showInputDialog("Registros\n" + "1    -  Registro de Bombeiro\n" + "2    -  Registro de Edifício\n" + "3    -  Registro de Ocorrência\n" + "4    -  Editar\n" + "5    -  Sair"));
+//        while (escolha != 5) {
+//            switch (escolha) {
+//                case 1:
+//            }
+//        }
+//    }
     public void cadastrar() {
         solicitarInformacao(atual);
         atual++;
+    }
+
+    public void editar() {
+        String busca = JOptionPane.showInputDialog("Digite o nome do edificio para editar: ");
+        for (int i = 0; i < atual; i++) {
+            if (nomeEdificio[i].equalsIgnoreCase(busca)) {
+                solicitarInformacao(i);
+                return;
+            }
+            JOptionPane.showMessageDialog(null, "Nome não encontrado.");
+        }
     }
 
     public void solicitarInformacao(int posicao) {
@@ -36,9 +51,9 @@ public class RegistroEdificio {
         cep[posicao] = JOptionPane.showInputDialog(nome[posicao] + " digite o CEP do edificio: ");
         cnpj[posicao] = JOptionPane.showInputDialog(nome[posicao] + " digite o CNPJ do edificio: ");
         edificio[posicao] = JOptionPane.showInputDialog(nome[posicao] + " digite o tipo de edificio: ");
-        
 
     }
+
     public void apresentarInformacao(int posicao) {
         JOptionPane.showMessageDialog(null,
                 "Nome do proprietario: " + nome[posicao]
@@ -49,7 +64,6 @@ public class RegistroEdificio {
                 + "CNPJ do registro: " + cnpj[posicao]
                 + "Tipo de edificio: " + edificio[posicao]);
 
-}
+    }
 
-}
 }
